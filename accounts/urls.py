72805c5cac3bfb1b94caee4646466af6079cf3cc
ahmadismail764 <ahmedeldeehy30305@gmail.com>
2026-06-07 01:21:29 +0300

    a few changes, committed very late

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
-from accounts.views import UserListView
+from accounts.views import UserViewSet
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'users', UserViewSet)
 
 urlpatterns = [
     # Login and stuff
     path('auth/login', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('users/', UserListView.as_view()),
 ]
+
+urlpatterns += router.urls
--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from accounts.models import User
 from accounts.serializers import UserSerializer
 
-class UserListView(generics.ListAPIView):
+# Endpoints provided by this view:
+# - GET    : returns a list of users (list endpoint)
+# - HEAD   : returns headers for the list endpoint
+# - OPTIONS: returns allowed HTTP methods and metadata for the endpoint
+# class UserListView(generics.ListAPIView):
+#     queryset = User.objects.all()
+#     serializer_class = UserSerializer
+#     pagination_class = None
+
+# Endpoints provided by this view:
+# - GET    : returns a list of users (list endpoint)
+# - HEAD   : returns headers for the list endpoint
+# - OPTIONS: returns allowed HTTP methods and metadata for the endpoint
+# - POST   : creates a new user (create endpoint)
+# - GET    : returns a single user by id (retrieve endpoint)
+# - PUT    : updates a single user by id (update endpoint)
+# - PATCH  : partially updates a single user by id (partial_update endpoint)
+# - DELETE : deletes a single user by id (destroy endpoint)
+class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    pagination_class = None 
